b13657091725ff6046bf27371385e23c893fa94a
Jeison Higuita Sanchez <jeisonhs93@gmail.com>
2017-02-05 16:33:43 -0500

    Create atom.backup.sh

---
+++ atom.backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Atom settings
+# I'm using auto sync form packages and more with [syncs-settings](http://atom.io/packages/sync-settings).
+
+# https://gist.github.com/jhta/f89a3a585d54e5972e9a27896a26d5ba
+# gist id: f89a3a585d54e5972e9a27896a26d5ba
+# token 1343f93715ace0f04ce5ed448ee8b48bc8b874eb
+apm install sync-settings
+
+# for recover the data, in the command pallete 'sync-settings:fork'
