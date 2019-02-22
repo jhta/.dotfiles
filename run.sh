ec43fb83a7a458bff70598d6431dc685995630e3
Jeison Higuita Sanchez <jeisonhs93@gmail.com>
2019-02-22 15:34:36 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,12 @@ ln -sf "$BASEDIR/vimrc.local" "$HOME/.vimrc.local"
 
 echo "Symbolic links created for .zshrc and .vimrc.local"
 
+#install fira code
+brew tap caskroom/fonts
+brew cask install font-fira-code
+
+echo "Fira code font installed"
+
 ln -sf "$BASEDIR/node.zsh-theme" "$HOME/.oh-my-zsh/themes/node.zsh-theme"
 echo "__________________________________"
 echo "Symbolic link added for node.zsh-theme"
